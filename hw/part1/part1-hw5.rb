857ffd563eeb618e8cd03be341484c02e6842e13
dobestan <dobestan@gmail.com>
2014-01-13 16:29:10 +0900

    finish part1 hw

--- hw/part1/part1-hw5.rb
+++ hw/part1/part1-hw5.rb
@@ -1,9 +1,11 @@
 def string_match(names, char)
-
+  match_list = Hash.new 
+  names.each do |name|
+    match_list[name] = name.count(char) if name.chr == char
+  end
+  return match_list
 end
 
 #execute
-string_match(["anteater","elephant","aardvark","dog","antelope"],"a")
-puts "-"*20
-stringMatch(["anteater","elephant","aardvark","dog","antelope"],"t")
-
+puts string_match(["anteater","elephant","aardvark","dog","antelope"],"a")
+puts string_match(["anteater","elephant","aardvark","dog","antelope"],"t")
