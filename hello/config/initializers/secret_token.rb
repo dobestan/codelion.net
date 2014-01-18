f9cb581f7faf17ee9b39126fa9424f6964c7dd9e
dobestan <dobestan@gmail.com>
2014-01-18 21:10:05 +0900

    add index.html +a

---
+++ hello/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hello::Application.config.secret_key_base = '410a51280eef1a82c4468eabd3d348244bffd6a006708ad329c1157802f3ac5bf6e506ab42c10dffa66194ad53a69f4787f5f44999622b378e3cff5f4d731b06'
