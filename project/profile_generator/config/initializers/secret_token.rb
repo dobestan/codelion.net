f1d3530cba4ee98f6b33d39130d807da45086c54
dobestan <dobestan@gmail.com>
2014-01-19 15:04:43 +0900

    INIT profile_generator

---
+++ project/profile_generator/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProfileGenerator::Application.config.secret_key_base = '2c9477edf878248c59dd3dc9a880347f2fb78aafa92e1b77b16cf03becf196eae11dec79a5ae24d5aa4c4f82218a4f74e737450aaaf597c78aa0e38c880dcbc6'
---
+++ project/profile_generator/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProfileGenerator::Application.config.session_store :cookie_store, key: '_profile_generator_session'
