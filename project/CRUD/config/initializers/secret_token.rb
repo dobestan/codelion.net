a528ada9642d56c99b3111b626d9da82e3cc9c74
dobestan <dobestan@gmail.com>
2014-01-26 17:26:35 +0900

    add new CRUD project

---
+++ project/CRUD/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CRUD::Application.config.secret_key_base = '191e3148522dde3adba7e08ba10a0d905b5a7a65e27d8767dcdc875c70c3977aba1f66a4951880b3e200521eebb44fcb84eb7cd59fb9a51d620426a82afac2ea'
